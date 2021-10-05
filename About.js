97d809f15b9f32faff2c4ddd7055086ae57d7fbb
minchulc <minchulc@uci.edu>
2021-10-05 11:20:59 -0700

    Add files via upload

---
+++ About.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./About.css"
+
+function About (pros){
+
+    return (
+        <div className = "about__container">
+            <span>
+                "about movies "
+            </span>
+        </div>
+    );
+}
+
+export default About;
